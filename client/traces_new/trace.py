4af7093bca686d773e2754f9cc8ecc3b2c775b6d
ginger0106 <840909love@163.com>
2019-09-28 20:45:42 +0800

    2.0

--- client/traces_new/trace.py
+++ client/traces_new/trace.py
@@ -10,7 +10,7 @@ def region():
     for region in os.listdir(PATH):
         if region.startswith('re'):
             path = os.path.join(PATH,region)
-            for num in range(8):
+            for num in range(6):
 
                 content_path = os.path.join(path,f'content_{num}.npy')
                 print(content_path)
@@ -29,24 +29,16 @@ def content():
 
                 content_path = os.path.join(path,f'content_{num}.npy')
                 # print(content_path)
-                trace_lst.append(np.load(content_path)[:60])
-        trace[num] = (trace_lst)
+                trace_lst.append(np.load(content_path)[:])
+        trace[num] = np.sum(trace_lst)
         trace_lst = []
-    # print(trace)
-    return trace
+    print(trace)
 # content()
 
 def gen_trace():
-    trace_lst = [20]*720
+    trace_lst = [6]*20+[10]*20+[2]*680
     print(trace_lst)
-    np.save('/home/ginger/a2_auto/client/traces_ori/region_0/content_9.npy',trace_lst)
-#
-# t = content()
-# print(t[0])
-# def change_trace():
-#     for file in os.listdir(PATH):
-#
+    np.save('/home/ginger/a2_auto/client/traces/region_0/content_8.npy',trace_lst)
 
+content()
 
-
-gen_trace()
